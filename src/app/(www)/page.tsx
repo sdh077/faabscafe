a9f134b3a3f9b8f77f0d96b197e17379678a9954
shindaeho <sdh077@naver.com>
2024-04-23 15:26:55 +0900

    a 위치

--- src/app/(www)/page.tsx
+++ src/app/(www)/page.tsx
@@ -10,12 +10,13 @@ export default async function Home() {
       <div className='h-[100vh] w-[270px] bg-white'>
         <div className='md:w-[200px] text-center absolute text-black py-6 md:pt-100 z-[11] right-0 top-0 md:left-0 md:top-[170px]'>
           {navItems.map((item, i) =>
-            <div className='mb-6' data-aos="fade-right" data-aos-duration={500 * (i + 1)} key={item.name} >
-              <Ripple >
-                <a className='text-black text-lg mb-30' href={item.link}>{item.name}
-                </a>
-              </Ripple>
-            </div>
+            <a className='text-black text-lg mb-30' href={item.link} key={item.name}>
+              <div className='mb-6' data-aos="fade-right" data-aos-duration={500 * (i + 1)}  >
+                <Ripple >
+                  {item.name}
+                </Ripple>
+              </div>
+            </a>
           )}
         </div>
       </div>
